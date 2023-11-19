976cde0462dacac14c9eabe9c49d323eaae00192
VanDerRomio <master1988roma@gmail.com>
2023-11-19 17:30:43 +0100

    added routs and empty controllers

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,34 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use \App\Http\Controllers\Api\TaskController;
+use \App\Http\Controllers\Api\UserController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('api')
+    ->group(function() {
+        Route::controller(TaskController::class)
+            ->prefix('tasks')
+            ->name('.tasks')
+            ->group(function () {
+                Route::get('/',         'index');
+                Route::post('/',        'store');
+                Route::get('/{id}',     'show');
+                Route::put('/{id}',     'update');
+                Route::delete('/{id}',  'delete');
+            });
+
+        Route::controller(UserController::class)
+            ->prefix('users')
+            ->name('.users')
+            ->group(function () {
+                Route::get('/',         'index');
+                Route::post('/',        'store');
+                Route::get('/{id}',     'show');
+                Route::put('/{id}',     'update');
+                Route::delete('/{id}',  'delete');
+            });
+    });
